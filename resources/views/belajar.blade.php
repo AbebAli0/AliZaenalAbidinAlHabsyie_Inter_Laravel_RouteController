01b78ea8f08ce8066a7d52ba1005e0cf74861b61
ali <aberetro123@gmail.com>
2024-02-21 15:04:07 +0700

    first commit

---
+++ resources/views/belajar.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ini view pertama saya bang</title>
+</head>
+<body>
+    Belajar Controller laravel sulit
+</body>
+</html>
